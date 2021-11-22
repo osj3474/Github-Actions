0754319614b1deb6a402fccec07289b9310bfb27
osj3474 <handong3474@gmail.com>
2021-11-22 21:37:49 +0900

    Update sample api

---
+++ src/main/java/action/action/api/HomeController.java
@@ -0,0 +1,12 @@
+package action.action.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping()
+    public String test(){
+        return "배포 테스트";
+    }
+}
