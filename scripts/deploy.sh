0124d469bad8a38b57af14d931a8608734ffc2cb
osj3474 <handong3474@gmail.com>
2021-12-06 08:26:54 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@
 REPOSITORY=/home/ubuntu/sangjin-deploy
 cd $REPOSITORY
 
-APP_NAME=CDapp
+APP_NAME=actions
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
